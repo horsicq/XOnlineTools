d6548fcc83bda5bc1876ed442a9251d4f8cfbe6d
horsicq <horsicq@gmail.com>
2022-10-22 04:24:21 +0200

    Fix: 2022-10-22

--- xvirustotal.cpp
+++ xvirustotal.cpp
@@ -26,7 +26,7 @@ XVirusTotal::XVirusTotal(QObject *pParent)
 
 }
 
-QJsonDocument XVirusTotal::getFileInfo(QString sHash, bool *pbIsNotFound)
+QJsonDocument XVirusTotal::getFileInfo(QString sHash,bool *pbIsNotFound)
 {
     return QJsonDocument::fromJson(sendRequest(RTYPE_GETFILEINFO,sHash,nullptr,pbIsNotFound));
 }
