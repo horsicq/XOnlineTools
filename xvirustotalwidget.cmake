c1802e8a83f97f6511f5d005cc8ed5b06428adfb
horsicq <horsicq@gmail.com>
2024-02-28 19:06:35 +0100

    Update file(s): 2024-02-28

--- xvirustotalwidget.cmake
+++ xvirustotalwidget.cmake
@@ -5,12 +5,18 @@ include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
 set(XVIRUSTOTALWIDGET_SOURCES
     ${XFORMATS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xonlinetools.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xonlinetools.h
     ${CMAKE_CURRENT_LIST_DIR}/xvirustotal.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xvirustotal.h
     ${CMAKE_CURRENT_LIST_DIR}/xvirustotalwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xvirustotalwidget.h
     ${CMAKE_CURRENT_LIST_DIR}/xvirustotalwidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/xonlinetoolsoptionswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xonlinetoolsoptionswidget.h
     ${CMAKE_CURRENT_LIST_DIR}/xonlinetoolsoptionswidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/xonlinetoolsdialogprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xonlinetoolsdialogprocess.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogxvirustotal.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxvirustotal.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogxvirustotal.ui
 )
